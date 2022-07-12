c1ab3c58a596360c0fe529ad237fdc167abd0c3f
Nabih <nabih@fuzzinglabs.com>
2022-07-12 12:58:36 +0200

    [TEST] fix testsuit

--- tests/json_to_txt.sh
+++ tests/json_to_txt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source ~/cairo_venv/bin/activate
-for filename in tests/json_files/*.json; do
+for filename in tests/json_test_files/*.json; do
     name=$(basename $filename)
     name="${name%%.*}"
     echo $name
-    python3 __main__.py cairo -file tests/json_files/"$name".json > tests/ref_files/"$name".txt
+    python3 __main__.py -file tests/json_test_files/"$name".json -vvv > tests/ref_files/"$name".txt
 done
