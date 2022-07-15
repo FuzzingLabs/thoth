e29f6452e8bcc735503214b3fcf18897669d869e
Nabih <nabih@fuzzinglabs.com>
2022-07-16 00:31:33 +0200

    [TEST] add test to check if all json files were tested

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 import unittest
+import glob
+
 from disassembler import Disassembler
 
 class TestDisassembler(unittest.TestCase):
+    
+    def test_all_files_tested(self):
+        all_test = glob.glob("./tests/json_files/*")
+        number_of_tests = len([method for method in dir(TestDisassembler) if method.startswith('test_')])
+        self.assertEqual(len(all_test), number_of_tests - 1)
+    
     def test_cairo_return(self):
         file = open("./tests/json_files/cairo_return.json", "r")
         disassembler = Disassembler([file])
@@ -11,7 +19,6 @@ class TestDisassembler(unittest.TestCase):
         self.assertEqual(analytics["builtins"], "0")
         self.assertEqual(analytics["decorators"], [])
         self.assertEqual(analytics["call_nbr"], "2")
-
         
     def test_FILENAME(self):
         """
