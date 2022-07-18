80ed872d63268399e1f247fe63a6976b379e27dc
Nabih <nabih@fuzzinglabs.com>
2022-07-18 15:10:41 +0200

    [PACKAGE] update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='Thoth',
-    version='0.1.0',    
-    description='Cairo language disassembler',
-    url='https://github.com/shuds13/pyexample',
-    author='FuzzingLabs',
-    author_email='contact@fuzzinglabs.com',
-    license='BSD 2-clause',
-    packages=['thoth'],
-    install_requires=['graphviz',                 
-                      ],
-
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+    name="Thoth",
+    version="0.1.0",
+    description="Cairo language disassembler",
+    url="https://github.com/shuds13/pyexample",
+    author="FuzzingLabs",
+    author_email="contact@fuzzinglabs.com",
+    license="AGPL-3.0",
+    packages=["thoth"],
+    python_requires=">=3.7",
+    install_requires=[
+        "graphviz",
     ],
 )
