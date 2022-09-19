908bcff2dd2e030e00a65a50b0adb285b5a999e6
Rog3rSm1th <r0g3r5@protonmail.com>
2022-09-19 12:34:41 +0200

    Refactor main function and argument parsing

---
+++ thoth/arguments.py
@@ -0,0 +1,80 @@
+import argparse
+
+def parse_args() -> argparse.Namespace:
+    """Parse the program arguments
+
+    Returns:
+        argparse.Namespace: namespace containing arguments
+    """
+    parser = argparse.ArgumentParser(
+        add_help=True,
+        description="Cairo Disassembler",
+        epilog="The exit status is 0 for non-failures and -1 for failures.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    c = parser.add_argument_group("mandatory arguments")
+    c.add_argument(
+        "-f",
+        "-file",
+        "--file",
+        metavar="file",
+        type=argparse.FileType("r"),
+        nargs="+",
+        required=True,
+        help="Cairo compiler JSON",
+    )
+
+    m = parser.add_argument_group("optional arguments")
+    m.add_argument(
+        "-vvv",
+        "-verbose",
+        "--verbose",
+        action="store_true",
+        help="Print JSON with details of all instructions",
+    )
+    m.add_argument(
+        "-c",
+        "-call",
+        "--call",
+        action="store_true",
+        help="Print call flow graph",
+    )
+    m.add_argument(
+        "-g",
+        "-cfg",
+        "--cfg",
+        action="store_true",
+        help="Print control flow graph",
+    )
+    m.add_argument(
+        "-format",
+        "--format",
+        metavar="Format of the output file [png-svg-pdf]",
+        nargs="?",
+        choices=["pdf", "png", "svg"],
+        help="Format of the graphs",
+    )
+    m.add_argument(
+        "-color",
+        "--color",
+        action="store_true",
+        help="Print disassembler with color",
+    )
+    m.add_argument(
+        "-function",
+        "--function",
+        type=str,
+        required=False,
+        help="Print disassembler with color",
+    )
+    m.add_argument(
+        "-a",
+        "-analytics",
+        "--analytics",
+        action="store_true",
+        help="Dump a Json file containing debug information",
+    )
+    m.add_argument("-d", "-decomp", "--decomp", action="store_true")
+    
+    return parser.parse_args()
--- thoth/thoth.py
+++ thoth/thoth.py
@@ -1,92 +1,8 @@
-#!/usr/bin/env python3
-
-import argparse
 import os
-from thoth import utils
+from .arguments import parse_args
 from .disassembler import Disassembler
 
-
-def parse_args():
-    """Parse the program arguments
-
-    Returns:
-        list: list containing arguments
-    """
-    parser = argparse.ArgumentParser(
-        add_help=True,
-        description="Cairo Disassembler",
-        epilog="The exit status is 0 for non-failures and -1 for failures.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-
-    c = parser.add_argument_group("mandatory arguments")
-    c.add_argument(
-        "-f",
-        "-file",
-        "--file",
-        metavar="file",
-        type=argparse.FileType("r"),
-        nargs="+",
-        required=True,
-        help="Cairo compiler JSON",
-    )
-
-    m = parser.add_argument_group("optional arguments")
-    m.add_argument(
-        "-vvv",
-        "-verbose",
-        "--verbose",
-        action="store_true",
-        help="Print JSON with details of all instructions",
-    )
-    m.add_argument(
-        "-c",
-        "-call",
-        "--call",
-        action="store_true",
-        help="Print call flow graph",
-    )
-    m.add_argument(
-        "-g",
-        "-cfg",
-        "--cfg",
-        action="store_true",
-        help="Print control flow graph",
-    )
-    m.add_argument(
-        "-format",
-        "--format",
-        metavar="Format of the output file [png-svg-pdf]",
-        nargs="?",
-        choices=["pdf", "png", "svg"],
-        help="Format of the graphs",
-    )
-    m.add_argument(
-        "-color",
-        "--color",
-        action="store_true",
-        help="Print disassembler with color",
-    )
-    m.add_argument(
-        "-function",
-        "--function",
-        type=str,
-        required=False,
-        help="Print disassembler with color",
-    )
-    m.add_argument(
-        "-a",
-        "-analytics",
-        "--analytics",
-        action="store_true",
-        help="Dump a Json file containing debug information",
-    )
-    m.add_argument("-d", "-decomp", "--decomp", action="store_true")
-
-    return parser.parse_args()
-
-
-def main():
+def main() -> int:
     """Main function of Thoth
 
     Returns:
@@ -103,8 +19,10 @@ def main():
         disassembler.decompiler()
     else:
         disassembler.print_disassembly()
+    
     filename = os.path.basename(args.file[0].name).split(".")[0]
     format = "pdf" if args.format is None else str(args.format)
+
     # print call flow graph
     if args.call:
         disassembler.print_call_flow_graph(filename=filename, format=format)
@@ -118,9 +36,4 @@ def main():
     # print analytics
     if args.analytics:
         print(disassembler.analytics())
-
     return 0
-
-
-if __name__ == "__main__":
-    main()
