3c84462a9a46fe7a60e19e4d96dd8c57e317841a
Rog3rSm1th <r0g3r5@protonmail.com>
2022-09-20 10:36:45 +0200

    Clean decompiler code

--- thoth/app/decompiler/decompiler.py
+++ thoth/app/decompiler/decompiler.py
@@ -1,6 +1,9 @@
+from dis import Instruction
 import re
+from typing import List
 from thoth.app import utils
-
+from thoth.app.disassembler.function import Function
+from thoth.app.disassembler.instruction import Instruction
 
 class Decompiler:
     """
@@ -8,25 +11,25 @@ class Decompiler:
 
     decompile bytecodes
     """
-
-    def __init__(self, functions):
-        self.tab = 1
+    def __init__(self, functions: List[Function]) -> None:
+        self.tab_count = 1
         self.end_else = []
-        self.ap = []
         self.ifcount = 0
         self.end_if = None
         self.functions = functions
         self.decompiled_function = None
         self.return_values = None
 
-    def _handle_assert_eq_decomp(self, instruction):
+    def _handle_assert_eq_decomp(self, instruction: Instruction) -> str:
         """Handle the ASSERT_EQ opcode
 
         Returns:
             String: The formated ASSERT_EQ instruction
         """
+        source_code = ""
+
         OPERATORS = {"ADD": "+", "MUL": "*"}
-        decomp_str = ""
+
         if "OP1" in instruction.res:
             if "IMM" in instruction.op1Addr:
                 value = utils.value_to_string(
@@ -36,55 +39,56 @@ class Decompiler:
                     value = utils.field_element_repr(
                         int(instruction.imm), instruction.prime
                     )
-                decomp_str += self.print_instruction_decomp(
+                source_code += self.print_instruction_decomp(
                     f"# {utils.field_element_repr(int(instruction.imm), instruction.prime)} -> {value}",
                     end="\n",
                     color=utils.color.CYAN,
                 )
-                decomp_str += self.print_instruction_decomp(
+                source_code += self.print_instruction_decomp(
                     f"[{instruction.dstRegister}{instruction.offDest}] = {utils.field_element_repr(int(instruction.imm), instruction.prime)}",
                     color=utils.color.GREEN,
                 )
             elif "OP0" in instruction.op1Addr:
-                decomp_str += self.print_instruction_decomp(
+                source_code += self.print_instruction_decomp(
                     f"[{instruction.dstRegister}{instruction.offDest}] = [[{instruction.op0Register}{instruction.off1}]{instruction.off2}]",
                     color=utils.color.GREEN,
                 )
             else:
-                decomp_str += self.print_instruction_decomp(
+                source_code += self.print_instruction_decomp(
                     f"[{instruction.dstRegister}{instruction.offDest}] = [{instruction.op1Addr}{instruction.off2}]",
                     color=utils.color.GREEN,
                 )
         else:
             op = OPERATORS[instruction.res]
             if "IMM" not in instruction.op1Addr:
-                decomp_str += self.print_instruction_decomp(
+                source_code += self.print_instruction_decomp(
                     f"[{instruction.dstRegister}{instruction.offDest}] =  [{instruction.op0Register}{instruction.off1}] {op} [{instruction.op1Addr}{instruction.off2}]",
                     color=utils.color.GREEN,
                 )
             else:
-                decomp_str += self.print_instruction_decomp(
+                source_code += self.print_instruction_decomp(
                     f"[{instruction.dstRegister}{instruction.offDest}] = [{instruction.op0Register}{instruction.off1}] {op} {utils.field_element_repr(int(instruction.imm), instruction.prime)}",
                     color=utils.color.GREEN,
                 )
-        return decomp_str
+        return source_code
 
-    def _handle_nop_decomp(self, instruction):
+    def _handle_nop_decomp(self, instruction: Instruction) -> str:
         """Handle the NOP opcode
 
         Returns:
             String: The formated NOP instruction
         """
-        decomp_str = ""
+        source_code = ""
+
         if "REGULAR" not in instruction.pcUpdate:
             if instruction.pcUpdate == "JNZ":
-                decomp_str += (
+                source_code += (
                     self.print_instruction_decomp(f"if ", color=utils.color.RED)
                     + f"[AP{instruction.offDest}] == 0:"
                 )
                 self.tab += 1
                 self.ifcount += 1
-                ## Detect if there is an else later
+                # Detect if there is an else later
                 jump_to = int(
                     utils.field_element_repr(
                         int(instruction.imm), instruction.prime
@@ -98,14 +102,13 @@ class Decompiler:
                         if inst.pcUpdate != "JUMP_REL":
                             self.end_if = int(jump_to)
                             self.ifcount -= 1
-                # for inst in next_instructions:
             elif instruction.pcUpdate == "JUMP_REL":
                 if self.ifcount != 0:
-                    self.tab -= 1
-                    decomp_str += self.print_instruction_decomp(
+                    self.tab_count -= 1
+                    source_code += self.print_instruction_decomp(
                         "else:", color=utils.color.RED
                     )
-                    self.tab += 1
+                    self.tab_count += 1
                     self.end_else.append(
                         int(
                             utils.field_element_repr(
@@ -116,19 +119,20 @@ class Decompiler:
                     )
                     self.ifcount -= 1
                 else:
-                    decomp_str += self.print_instruction_decomp(
+                    source_code += self.print_instruction_decomp(
                         f"jmp rel {instruction.imm}"
                     )
-        return decomp_str
+        return source_code
 
-    def _handle_call_decomp(self, instruction):
+    def _handle_call_decomp(self, instruction: Instruction) -> str:
         """Handle the CALL opcode
 
         Returns:
             String: The formated CALL instruction
         """
         # Direct CALL or Relative CALL
-        decomp_str = ""
+        source_code = ""
+
         call_type = "call abs" if instruction.is_call_abs() else "call rel"
         if instruction.is_call_direct():
             offset = int(
@@ -152,7 +156,7 @@ class Decompiler:
                     if args != 1:
                         args_str += ", "
                     args -= 1
-                decomp_str += (
+                source_code += (
                     self.print_instruction_decomp(
                         f"{call_name[-1]}", color=utils.color.RED
                     )
@@ -161,74 +165,75 @@ class Decompiler:
             # CALL to a label
             # e.g. call rel (123)
             else:
-                decomp_str += self.print_instruction_decomp(
+                source_code += self.print_instruction_decomp(
                     f"{call_type} ({offset})", color=utils.color.RED
                 )
                 if str(offset) in instruction.labels:
-                    decomp_str += self.print_instruction_decomp(
+                    source_code += self.print_instruction_decomp(
                         f"# {instruction.labels[str(offset)]}",
                         color=utils.color.CYAN,
                     )
         # CALL
         # e.g. call rel [fp + 4]
         elif instruction.is_call_indirect():
-            decomp_str += self.print_instruction_decomp(
+            source_code += self.print_instruction_decomp(
                 f"{call_type} [{instruction.op1Addr}{instruction.off2}]"
             )
         else:
             raise NotImplementedError
+        return source_code
 
-        return decomp_str
-
-    def _handle_ret_decomp(self, last=False):
+    def _handle_ret_decomp(self, last: bool = False) -> str:
         """Handle the RET opcode
 
         Returns:
             String: The formated RET instruction
         """
-        decomp_str = ""
+        source_code = ""
+
         if self.return_values == None:
-            decomp_str += self.print_instruction_decomp(
+            source_code += self.print_instruction_decomp(
                 "ret", end="\n", color=utils.color.RED
             )
             if last:
-                self.tab -= 1
+                self.tab_count -= 1
         else:
             idx = len(self.return_values)
-            decomp_str += (
+            source_code += (
                 self.print_instruction_decomp("return", color=utils.color.RED)
                 + "("
             )
             while idx:
-                decomp_str += f"[ap-{idx}]"
+                source_code += f"[ap-{idx}]"
                 if idx != 1:
-                    decomp_str += ", "
+                    source_code += ", "
                 idx -= 1
-            decomp_str += ")\n"
+            source_code += ")\n"
         if last:
-            self.tab = 0
-            decomp_str += self.print_instruction_decomp(
+            self.tab_count = 0
+            source_code += self.print_instruction_decomp(
                 "end", color=utils.color.RED
             )
-        return decomp_str
+        return source_code
 
-    def _handle_hint_decomp(self, instruction, last=False):
+    def _handle_hint_decomp(self, instruction: Instruction) -> str:
         """Handle the hint
 
         Returns:
             String: The formated hint
         """
-        hints = instruction.hint.split("\n")
-        decomp_str = ""
-        decomp_str += self.print_instruction_decomp("%{ ", end="\n")
-        self.tab += 1
-        for hint in hints:
-            decomp_str += self.print_instruction_decomp(hint, end="\n")
-        self.tab -= 1
-        decomp_str += self.print_instruction_decomp("%} ", end="\n")
-        return decomp_str
+        source_code = ""
 
-    def print_build_code(self, instruction, last=False):
+        hints = instruction.hint.split("\n")
+        source_code += self.print_instruction_decomp("%{ ", end="\n")
+        self.tab_count += 1
+        for hint in hints:
+            source_code += self.print_instruction_decomp(hint, end="\n")
+        self.tab_count -= 1
+        source_code += self.print_instruction_decomp("%} ", end="\n")
+        return source_code
+
+    def print_build_code(self, instruction: Instruction, last: bool = False) -> str:
         """Read the instruction and print each element of it
 
         Raises:
@@ -237,19 +242,20 @@ class Decompiler:
         Returns:
             String: String containing the instruction line with the offset ...
         """
-        decomp_str = ""
+        source_code = ""
+        
         if instruction.id in instruction.labels:
-            decomp_str += self.print_instruction_decomp(
+            source_code += self.print_instruction_decomp(
                 f"\nLABEL : {instruction.labels[instruction.id]}",
                 color=utils.color.GREEN,
             )
         if instruction.hint:
-            decomp_str += self._handle_hint_decomp(instruction)
+            source_code += self._handle_hint_decomp(instruction)
 
         if "ASSERT_EQ" in instruction.opcode:
-            decomp_str += self._handle_assert_eq_decomp(instruction)
+            source_code += self._handle_assert_eq_decomp(instruction)
             if "REGULAR" not in instruction.apUpdate:
-                decomp_str += ";"
+                source_code += ";"
                 op = list(
                     filter(None, re.split(r"(\d+)", instruction.apUpdate))
                 )
@@ -263,22 +269,22 @@ class Decompiler:
                     )
                 )
                 for i in range(int(APval)):
-                    decomp_str += self.print_instruction_decomp(
-                        f"ap ++", tab=self.tab, color=utils.color.YELLOW
+                    source_code += self.print_instruction_decomp(
+                        f"ap ++", tab=self.tab_count, color=utils.color.YELLOW
                     )
                     if i != int(APval) - 1:
-                        decomp_str += "\n"
+                        source_code += "\n"
         elif "NOP" in instruction.opcode:
-            decomp_str += self._handle_nop_decomp(instruction)
+            source_code += self._handle_nop_decomp(instruction)
         elif "CALL" in instruction.opcode:
-            decomp_str += self._handle_call_decomp(instruction)
+            source_code += self._handle_call_decomp(instruction)
         elif "RET" in instruction.opcode:
-            decomp_str += self._handle_ret_decomp(last=last)
+            source_code += self._handle_ret_decomp(last=last)
         else:
             raise AssertionError
-        return decomp_str
+        return source_code
 
-    def print_instruction_decomp(self, data, color="", end="", tab=None):
+    def print_instruction_decomp(self, data: str, color: str = "", end: str = "", tab_count: int = None) -> str:
         """format the instruction
 
         Args:
@@ -289,32 +295,42 @@ class Decompiler:
         Returns:
             String: The formated Instruction
         """
-        tabulation = "    " * self.tab
-        if tab != None:
-            tabulation = "    " * tab
-        return color + tabulation + data + utils.color.ENDC + end
+        tabulation = "    "
 
-    def decompile_code(self):
-        res = ""
+        if tab_count is not None:
+            tabulations = tabulation * tab_count
+        else:
+            tabulations = tabulation * self.tab_count
+
+        decompiled_instruction = color + tabulation + data + utils.color.ENDC + end
+        return decompiled_instruction
+
+    def decompile_code(self) -> str:
+        source_code = ""
+
         for function in self.functions:
-            self.tab = 0
+            self.tab_count = 0
             count = 0
+            
             if function.is_import is False:
-                res += "\n"
+                source_code += "\n"
                 self.decompiled_function = function
                 self.return_values = function.ret
+
                 function.generate_cfg()
-                res += self.print_instruction_decomp(
+
+                source_code += self.print_instruction_decomp(
                     function.get_prototype(), end="\n", color=utils.color.BLUE
                 )
-                self.tab += 1
+                self.tab_count += 1
+
                 if function.cfg.basicblocks != []:
                     for block in function.cfg.basicblocks:
                         for instruction in block.instructions:
                             if int(instruction.id) == self.end_if:
                                 self.end_if = None
-                                self.tab -= 1
-                                res += self.print_instruction_decomp(
+                                self.tab_count -= 1
+                                source_code += self.print_instruction_decomp(
                                     "end", end="\n", color=utils.color.RED
                                 )
                             if self.end_else != []:
@@ -322,8 +338,8 @@ class Decompiler:
                                     if self.end_else[idx] == int(
                                         instruction.id
                                     ):
-                                        self.tab -= 1
-                                        res += self.print_instruction_decomp(
+                                        self.tab_count -= 1
+                                        source_code += self.print_instruction_decomp(
                                             "end",
                                             end="\n",
                                             color=utils.color.RED,
@@ -335,5 +351,5 @@ class Decompiler:
                                 instruction,
                                 last=(count == len(function.instructions)),
                             )
-                            res += instruction + "\n"
-        return res
+                            source_code += instruction + "\n"
+        return source_code
