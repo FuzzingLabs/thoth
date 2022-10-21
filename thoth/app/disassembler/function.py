afcc72afe0157406009fae675e30e118c9548a74
Rog3rSm1th <r0g3r5@protonmail.com>
2022-10-21 15:29:20 +0200

    Implement cyclomatic complexity computation

--- thoth/app/disassembler/function.py
+++ thoth/app/disassembler/function.py
@@ -3,6 +3,7 @@ from typing import List
 from thoth.app import utils
 from thoth.app.cfg.cfg import CFG
 from thoth.app.disassembler.instruction import Instruction
+from thoth.app.utils import Kosaraju
 
 
 class Function:
@@ -54,6 +55,47 @@ class Function:
         self.cfg = None
         self.labels = labels
         self._generate_instruction()
+
+    @property
+    def cyclomatic_complexity(self) -> int:
+        """
+        Compute the cyclomatic complexity of the function
+        M = E - N + 2P
+        E: the number of edges of the graph
+        N: the number of nodes of the graph
+        P: the number of connected components
+        """
+        if self.cfg is None:
+            self.generate_cfg()
+
+        edges_count = 0
+        nodes_count = len(self.cfg.basicblocks)
+
+        # Compute edges count
+        for basic_block in self.cfg.basicblocks:
+            edges_count += len(basic_block.edges_offset)
+
+        graph = []
+        basic_blocks = self.cfg.basicblocks
+        # Generate a list of edges from the CFG
+        for i in range(len(basic_blocks)):
+            graph.append([])
+            for edge in basic_blocks[i].edges_offset:
+                try:
+                    destination_offset = edge.destination
+                    destination_basic_block = [
+                        bb for bb in basic_blocks if bb.start_offset == destination_offset
+                    ][0]
+                    destination_node = basic_blocks.index(destination_basic_block)
+                    graph[i].append(destination_node)
+                except:
+                    pass
+
+        # Compute the number of the CFG connected components
+        connected_components_count = len(Kosaraju(graph).connected_components())
+
+        cyclomatic_complexity = edges_count - nodes_count + 2 * connected_components_count
+        return cyclomatic_complexity
 
     def _generate_instruction(self) -> List[Instruction]:
         """Create a list of the instruction with its datas
