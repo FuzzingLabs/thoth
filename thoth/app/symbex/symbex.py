58a52b4b95e847292133cacf7f560365146d1097
Rog3rSm1th <r0g3r5@protonmail.com>
2022-11-08 15:19:29 +0100

    Assign variables values in the z3 solver

--- thoth/app/symbex/symbex.py
+++ thoth/app/symbex/symbex.py
@@ -1,7 +1,7 @@
 from typing import List, Tuple
 from z3 import *
 
-from thoth.app.decompiler.variable import Variable
+from thoth.app.decompiler.variable import OperandType, Operator, Variable, VariableValueType
 
 
 class SymbolicExecution:
@@ -19,7 +19,55 @@ class SymbolicExecution:
         Create z3 ArithRef objects from the program memory
         """
         for variable in self.variables:
-            self.z3_variables.append(variable.name)
+            self.z3_variables.append(Int(variable.name))
+
+    def _create_operations(self) -> None:
+        """
+        Assign variables value in the z3 solver
+        """
+        for variable in self.variables:
+            if variable.value is None:
+                continue
+            if variable.value.type == VariableValueType.FUNCTION_CALL:
+                continue
+            if variable.value.type == VariableValueType.ADDRESS:
+                continue
+
+            assigned_variable = [v for v in self.z3_variables if variable.name == str(v)][0]
+            operation = variable.value.operation
+            # Simple variable assignation
+            if len(operation) == 1:
+                if operation[0].type == OperandType.INTEGER:
+                    self.solver.add(assigned_variable == int(operation[0].value))
+                else:
+                    assigned_variable_value = [
+                        v for v in self.z3_variables if str(v) == operation[0].value[0]
+                    ][0]
+                    self.solver.add(assigned_variable == assigned_variable_value)
+
+            # Assignation with an operation
+            else:
+                # First operand
+                if operation[0].type == OperandType.INTEGER:
+                    first_operand = int(operation[0].value)
+                else:
+                    first_operand = [
+                        v for v in self.z3_variables if str(v) == operation[0].value[0]
+                    ][0]
+
+                # Second operand
+                if operation[2].type == OperandType.INTEGER:
+                    second_operand = int(operation[2].value)
+                else:
+                    second_operand = [
+                        v for v in self.z3_variables if str(v) == operation[0].value[0]
+                    ][0]
+
+                # Operation
+                if operation[1] == Operator.ADDITION:
+                    self.solver.add(assigned_variable == first_operand + second_operand)
+                else:
+                    self.solver.add(assigned_variable == first_operand * second_operand)
 
     def _add_variable(self, name: str) -> None:
         """
@@ -32,4 +80,5 @@ class SymbolicExecution:
         Generate a list of testcases allowing to cover all the possible paths of a function
         """
         self._create_z3_variables()
+        self._create_operations()
         return ()
