21945167cc47890c48d3ea4a1de9b95ee119993c
Rog3rSm1th <r0g3r5@protonmail.com>
2022-11-08 13:57:17 +0100

    Create z3 ArithRef objects from the program memory

--- thoth/app/symbex/symbex.py
+++ thoth/app/symbex/symbex.py
@@ -1,6 +1,7 @@
+from typing import List, Tuple
 from z3 import *
-from typing import Tuple
-from thoth.app.disassembler.function import Function
+
+from thoth.app.decompiler.variable import Variable
 
 
 class SymbolicExecution:
@@ -8,12 +9,27 @@ class SymbolicExecution:
     Symbolic execution class
     """
 
-    def __init__(self, function: Function) -> None:
-        self.function = function
+    def __init__(self, variables: List[Variable]) -> None:
         self.solver = z3.Solver()
+        self.variables: List[Variable] = variables
+        self.z3_variables: List[ArithRef] = []
+
+    def _create_z3_variables(self) -> None:
+        """
+        Create z3 ArithRef objects from the program memory
+        """
+        for variable in self.variables:
+            self.z3_variables.append(variable.name)
+
+    def _add_variable(self, name: str) -> None:
+        """
+        Add a variable to the global z3 variables list
+        """
+        self.variables.append(Int(name))
 
     def _generate_test_cases(self) -> Tuple[Tuple[str, int]]:
         """
         Generate a list of testcases allowing to cover all the possible paths of a function
         """
-        return
+        self._create_z3_variables()
+        return ()
