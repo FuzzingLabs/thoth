b63857552bb0aa83829a4b247ca9568384a204d5
Rog3rSm1th <r0g3r5@protonmail.com>
2022-12-02 11:27:54 +0100

    Create the Sierra objects structure

---
+++ sierra/objects.py
@@ -0,0 +1,42 @@
+class SierraObject:
+    """
+    Base object from which all Sierra objects are inherited from
+    """
+
+    pass
+
+
+class SierraType(SierraObject):
+    """
+    Sierra Type class
+    """
+
+    def __init__(self) -> None:
+        pass
+
+
+class SierraLibFunc(SierraObject):
+    """
+    Sierra LibFunc class
+    """
+
+    def __init__(self) -> None:
+        pass
+
+
+class SierraStatement(SierraObject):
+    """
+    Sierra Statement class
+    """
+
+    def __init__(self) -> None:
+        pass
+
+
+class SierraFunction(SierraObject):
+    """
+    Sierra Function class
+    """
+
+    def __init__(self) -> None:
+        pass
