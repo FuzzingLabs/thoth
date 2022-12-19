a6223bed45526bb9bbc22b0b1299fac44620036d
Rog3rSm1th <r0g3r5@protonmail.com>
2022-12-19 18:00:28 +0100

    Implement the sierra abstract analyzer

---
+++ sierra/analyzer/abstract_analyzer.py
@@ -0,0 +1,148 @@
+from enum import Enum
+from typing import Dict, List
+from sierra.parser.parser import SierraParser
+
+
+class colors:
+    HEADER = "\033[95m"
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    ENDC = "\033[0m"
+
+
+class PrecisionClassification(Enum):
+    """
+    Level of precision of an analyzer
+    """
+
+    LOW = 0
+    MEDIUM = 1
+    HIGH = 2
+
+
+class ImpactClassification(Enum):
+    """
+    Level of impact of a security detector
+    Analytics/Optimization analyzers have a NONE Impact by default
+    """
+
+    INFORMATIONAL = 0
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+
+
+class CategoryClassification(Enum):
+    """
+    Analyzer category
+    """
+
+    ANALYTICS = 0
+    OPTIMIZATION = 1
+    SECURITY = 2
+
+    UNIMPLEMENTED = 999
+
+
+category_classification_colors: Dict[CategoryClassification, str] = {
+    CategoryClassification.ANALYTICS: colors.GREEN,
+    CategoryClassification.OPTIMIZATION: colors.CYAN,
+    CategoryClassification.SECURITY: colors.RED,
+}
+
+category_classification_text: Dict[CategoryClassification, str] = {
+    CategoryClassification.ANALYTICS: "Analytics",
+    CategoryClassification.OPTIMIZATION: "Optimization",
+    CategoryClassification.SECURITY: "Security",
+}
+
+impact_classification_colors: Dict[ImpactClassification, str] = {
+    ImpactClassification.INFORMATIONAL: colors.CYAN,
+    ImpactClassification.LOW: colors.GREEN,
+    ImpactClassification.MEDIUM: colors.YELLOW,
+    ImpactClassification.HIGH: colors.RED,
+}
+
+impact_classification_text: Dict[ImpactClassification, str] = {
+    ImpactClassification.INFORMATIONAL: "Informational",
+    ImpactClassification.LOW: "Low",
+    ImpactClassification.MEDIUM: "Medium",
+    ImpactClassification.HIGH: "High",
+}
+
+
+class SierraAbstractAnalyzer:
+    """
+    Base class from which all sierra analyzers inherit
+    """
+
+    # Analyzer name
+    NAME = ""
+    # Name to use in CLI to select the analyzer
+    ARGUMENT = ""
+    # Analyzer description
+    HELP = ""
+    # Analyzer impact
+    IMPACT: ImpactClassification = ImpactClassification.INFORMATIONAL
+    # Analyzer precision
+    PRECISION: PrecisionClassification = PrecisionClassification.HIGH
+    # Analyzer category
+    CATEGORY: CategoryClassification = CategoryClassification.UNIMPLEMENTED
+
+    def __init__(self, program: SierraParser) -> None:
+        """
+        Detector initialization
+        """
+        self.program = program
+        self.result: List[str] = []
+        self.detected = False
+
+    def _detect(self) -> bool:
+        """
+        Run analyzer on the contract
+        """
+        pass
+
+    @classmethod
+    def _print_help(cls, color: bool = True) -> None:
+        """
+        Print the analyzer documentation
+        """
+        category_color = category_classification_colors[cls.CATEGORY] if color else ""
+        help = "[%s] %s%s%s <%s> - " % (
+            category_classification_text[cls.CATEGORY],
+            category_color,
+            cls.NAME,
+            colors.ENDC,
+            cls.ARGUMENT,
+        )
+        help += "%s" % cls.HELP
+        print(help)
+
+    def print(self) -> bool:
+        """
+        Print the detector result
+        """
+        if not self.detected:
+            return False
+
+        # Impact
+        impact = ""
+        if self.CATEGORY == CategoryClassification.SECURITY:
+            impact_color = impact_classification_colors[self.IMPACT] if self.color else ""
+            impact = "(%s)" % (impact_color + impact_classification_text[self.IMPACT] + colors.ENDC)
+
+        category_color = category_classification_colors[self.CATEGORY] if self.color else ""
+        title = "[%s%s%s] %s %s" % (
+            category_color,
+            category_classification_text[self.CATEGORY],
+            colors.ENDC,
+            self.NAME,
+            impact,
+        )
+        print(title)
+        for result in self.result:
+            print("  -  %s" % result)
+        return True
