d74f17a87b1d21677f29636d5433e1c2d7987a97
Rog3rSm1th <r0g3r5@protonmail.com>
2023-04-27 17:22:22 +0200

    Add commands for JSON artifacts

--- vscode-extension/README.md
+++ vscode-extension/README.md
@@ -1,71 +1,35 @@
-# thoth-vscode README
+## Thoth Visual Studio code extension
 
-This is the README for your extension "thoth-vscode". After writing up a brief description, we recommend including the following sections.
+The thoth extension allows you to use thoth functions directly in Visual Studio Code.
 
-## Features
+### How to install
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+#### Create the **.vsix** package
+```bash
+# Install vscm in npm 
+npm install -g vsce
 
-For example if there is an image subfolder under your extension project workspace:
+# Clone and open the repository
+git clone https://github.com/FuzzingLabs/thoth.git
+cd ./thoth/github-actions
 
-\!\[feature X\]\(images/feature-x.png\)
+# Output a 'thoth-vscode-x.x.x.vsix' in the folder
+vsce package
+```
+#### Install the extension
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+To install it on your Visual Studio Code, navigate to the extension page and click on the three-dot icon. Then select  "*Install from VSIX*" and specify the location of the .vsix file you created in the previous step.
 
-## Requirements
+### Usage
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+Open Command Palette using `CTRL + SHIFT + P` and type the following commands: 
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+- `>Run thoth-sierra decompiler` to output the decompiled version of a Sierra file.
+- `>Run thoth-sierra analyzers` to output the analyzers results on a Sierra file.
+- `>Run thoth-sierra CFG` to output the CFG of a Sierra file.
+- `>Run thoth-sierra callgraph` to output the CallGraph of a Sierra file.
+- `>Run thoth decompiler` to output the decompiled version of a Cairo JSON artifact.
+- `>Run thoth disassembler` to output the CallGraph of a Cairo JSON artifact.
+- `>Run thoth analyzers` to output the analyzers results on a Cairo JSON artifact.
+- `>Run thoth CFG` to output the CFG of a Cairo JSON artifact.
+- `>Run thoth callgraph` to output the CallGraph of a Cairo JSON artifact.
--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -22,6 +22,7 @@ function replaceColors(data: string) {
     "\x1b[91m": "<span style='color:#D21312' >",
     "\x1b[0m": "</span>",
     "\x1b[1m": "",
+    "\x1b[36m": "",
     "\x1b[4m": "",
   };
 
@@ -39,18 +40,30 @@ function activate(context: { subscriptions: any[]; }) {
    * @param {vscode.ExtensionContext} context - The extension context object
    */
 
-  // Register commands
+  // Register thoth-sierra commands
   const runSierraDecompilerCommand = vscode.commands.registerCommand('extension.runThothSierraDecompiler', runThothSierraDecompiler);
   const runSierraAnalyzerCommand = vscode.commands.registerCommand('extension.runThothSierraAnalyzer', runThothSierraAnalyzer);
   const runSierraCFGCommand = vscode.commands.registerCommand('extension.runThothSierraCFG', runThothSierraCFG);
   const runSierraCallgraphCommand = vscode.commands.registerCommand('extension.runThothSierraCallGraph', runThothSierraCallGraph);
+
+  // Register thoth commands
+  const runThothDecompilerCommand = vscode.commands.registerCommand('extension.runThothDecompiler', runThothDecompiler);
+  const runThothDisassemblerCommand = vscode.commands.registerCommand('extension.runThothDisassembler', runThothDisassembler);
+  const runThothAnalyzerCommand = vscode.commands.registerCommand('extension.runThothAnalyzer', runThothAnalyzer);
+  const runThothCFGCommand = vscode.commands.registerCommand('extension.runThothCFG', runThothCFG);
+  const runThothCallGraphCommand = vscode.commands.registerCommand('extension.runThothCallGraph', runThothCallGraph);
 
   // Add commands to subscriptions array
   context.subscriptions.push(
     runSierraDecompilerCommand,
     runSierraAnalyzerCommand,
     runSierraCFGCommand,
-    runSierraCallgraphCommand
+    runSierraCallgraphCommand,
+    runThothDecompilerCommand,
+    runThothDisassemblerCommand,
+    runThothAnalyzerCommand,
+    runThothCFGCommand,
+    runThothCallGraphCommand
   );
 }
 
@@ -185,18 +198,18 @@ function runThothSierraCFG() {
   const onDiskPath = vscode.Uri.file(imagePath);
   const imageUri = panel.webview.asWebviewUri(onDiskPath).toString();
   console.log(imageUri);
-  output += `a<img src="${imageUri}" alt="${onDiskPath.path}"/>`;
+  output += `<img src="${imageUri}" alt="${onDiskPath.path}"/>`;
   
   panel.webview.html = output;
 
-  thothProcess.stderr.on('data', (data: string) => {
+  thothProcess.stderr.on('exit', (data: string) => {
     output += data;
     panel.webview.html = output;
   });
 
   thothProcess.on('close', (code: number) => {
     if (code !== 0) {
-      output += `Thoth Sierra Analyzer exited with code ${code}.`;
+      output += `Thoth Sierra CFG exited with code ${code}.`;
       panel.webview.html = output;
     }
   });
@@ -243,14 +256,271 @@ function runThothSierraCallGraph() {
   
   panel.webview.html = output;
 
-  thothProcess.stderr.on('data', (data: string) => {
+  thothProcess.stderr.on('exit', (data: string) => {
     output += data;
     panel.webview.html = output;
   });
 
   thothProcess.on('close', (code: number) => {
     if (code !== 0) {
-      output += `Thoth Sierra Analyzer exited with code ${code}.`;
+      output += `Thoth Sierra Callgraph exited with code ${code}.`;
+      panel.webview.html = output;
+    }
+  });
+
+  panel.onDidDispose(() => {
+    thothProcess.kill();
+  });
+}
+
+function runThothDecompiler() {
+  /**
+   * Runs Thoth Decompiler on the currently opened file
+   */
+
+  // Get the currently opened file
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    vscode.window.showErrorMessage("No active editor found.");
+    return;
+  }
+  const filePath = activeEditor.document.uri.fsPath;
+
+  // Create a new panel and set its content
+  const panel = vscode.window.createWebviewPanel(
+    'thothDecompiler',
+    'Thoth Decompiler',
+    vscode.ViewColumn.Beside,
+    {}
+  );
+
+  // Run the thoth command on the file and update the panel content
+  const { spawn } = require('child_process');
+  const thothProcess = spawn('thoth', ['local', filePath, '-d', '--color']);
+
+  let output = '';
+  thothProcess.stdout.on('data', (data: string) => {
+    data = replaceColors(data.toString());
+    output += `<pre><code>${data}</code></pre>`;
+    panel.webview.html = output;
+  });
+
+  thothProcess.stderr.on('data', (data: { toString: () => string; }) => {
+    output += data.toString();
+    panel.webview.html = output;
+  });
+
+  thothProcess.on('close', (code: number) => {
+    if (code !== 0) {
+      output += `Thoth Decompiler exited with code ${code}.`;
+      panel.webview.html = output;
+    }
+  });
+
+  panel.onDidDispose(() => {
+    thothProcess.kill();
+  });
+}
+
+function runThothDisassembler() {
+  /**
+   * Runs Thoth Disassembler on the currently opened file
+   */
+
+  // Get the currently opened file
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    vscode.window.showErrorMessage("No active editor found.");
+    return;
+  }
+  const filePath = activeEditor.document.uri.fsPath;
+
+  // Create a new panel and set its content
+  const panel = vscode.window.createWebviewPanel(
+    'thothDisassembler',
+    'Thoth Disassembler',
+    vscode.ViewColumn.Beside,
+    {}
+  );
+
+  // Run the thoth command on the file and update the panel content
+  const { spawn } = require('child_process');
+  const thothProcess = spawn('thoth', ['local', filePath, '-b', '--color']);
+
+  let output = '';
+  thothProcess.stdout.on('data', (data: string) => {
+    data = replaceColors(data.toString());
+    output += `<pre><code>${data}</code></pre>`;
+    panel.webview.html = output;
+  });
+
+  thothProcess.stderr.on('data', (data: { toString: () => string; }) => {
+    output += data.toString();
+    panel.webview.html = output;
+  });
+
+  thothProcess.on('close', (code: number) => {
+    if (code !== 0) {
+      output += `Thoth Disassembler exited with code ${code}.`;
+      panel.webview.html = output;
+    }
+  });
+
+  panel.onDidDispose(() => {
+    thothProcess.kill();
+  });
+}
+
+function runThothAnalyzer() {
+  /**
+   * Runs Thoth Analyzers on the currently opened file
+   */
+
+  // Get the currently opened file
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    vscode.window.showErrorMessage("No active editor found.");
+    return;
+  }
+  const filePath = activeEditor.document.uri.fsPath;
+
+  // Create a new panel and set its content
+  const panel = vscode.window.createWebviewPanel(
+    'thothAnalyzer',
+    'Thoth Analyzers',
+    vscode.ViewColumn.Beside,
+    {}
+  );
+
+  // Run the thoth command on the file and update the panel content
+  const { spawn } = require('child_process');
+  const thothProcess = spawn('thoth', ['local', filePath, '-a', '--color']);
+
+  let output = '';
+  thothProcess.stdout.on('data', (data: string) => {
+    data = replaceColors(data.toString());
+    output += `<pre><code>${data}</code></pre>`;
+    panel.webview.html = output;
+  });
+
+  thothProcess.stderr.on('data', (data: { toString: () => string; }) => {
+    output += data.toString();
+    panel.webview.html = output;
+  });
+
+  thothProcess.on('close', (code: number) => {
+    if (code !== 0) {
+      output += `Thoth Analyzers exited with code ${code}.`;
+      panel.webview.html = output;
+    }
+  });
+
+  panel.onDidDispose(() => {
+    thothProcess.kill();
+  });
+}
+
+function runThothCFG() {
+  /**
+   * Runs Thoth CFG on the currently opened file
+   */
+
+  // Get the currently opened file
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    vscode.window.showErrorMessage("No active editor found.");
+    return;
+  }
+  const filePath = activeEditor.document.uri.fsPath;
+
+  // Create a new panel and set its content
+  const panel = vscode.window.createWebviewPanel(
+    'thothCFG',
+    'Thoth CFG',
+    vscode.ViewColumn.Beside,
+    {
+      localResourceRoots: [vscode.Uri.file('/tmp/cfgoutput/')]
+    }
+  );
+
+  // Run the thoth command on the file and update the panel content
+  const { spawn } = require('child_process');
+  const thothProcess = spawn('thoth', ['local', filePath, '--cfg', '-output_cfg_folder', '/tmp/cfgoutput', '--format', 'png']);
+  
+  let output = '';
+
+  const fileNameWithExtension = filePath.split("/").pop(); 
+  const fileName = fileNameWithExtension.split(".")[0];
+  const imagePath = `/tmp/cfgoutput/${fileName}.gv.png`;
+  const onDiskPath = vscode.Uri.file(imagePath);
+  const imageUri = panel.webview.asWebviewUri(onDiskPath).toString();
+  output += `<img src="${imageUri}" alt="${onDiskPath.path}"/>`;
+  
+  panel.webview.html = output;
+
+  thothProcess.stderr.on('exit', (data: string) => {
+    output += data;
+    panel.webview.html = output;
+  });
+
+  thothProcess.on('close', (code: number) => {
+    if (code !== 0) {
+      output += `Thoth CFG exited with code ${code}.`;
+      panel.webview.html = output;
+    }
+  });
+
+  panel.onDidDispose(() => {
+    thothProcess.kill();
+  });
+}
+
+function runThothCallGraph() {
+  /**
+   * Runs Thoth Callgraph on the currently opened file
+   */
+
+  // Get the currently opened file
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    vscode.window.showErrorMessage("No active editor found.");
+    return;
+  }
+  const filePath = activeEditor.document.uri.fsPath;
+
+  // Create a new panel and set its content
+  const panel = vscode.window.createWebviewPanel(
+    'thothCallGraph',
+    'Thoth CallGraph',
+    vscode.ViewColumn.Beside,
+    {
+      localResourceRoots: [vscode.Uri.file('/tmp/callgraphoutput/')]
+    }
+  );
+
+  // Run the thoth command on the file and update the panel content
+  const { spawn } = require('child_process');
+  const thothProcess = spawn('thoth', ['local', filePath, '--call', '-output_callgraph_folder', '/tmp/callgraphoutput', '--format', 'png']);
+  
+  let output = '';
+
+  const fileNameWithExtension = filePath.split("/").pop(); 
+  const fileName = fileNameWithExtension.split(".")[0];
+  const imagePath = `/tmp/callgraphoutput/${fileName}.gv.png`;
+  const onDiskPath = vscode.Uri.file(imagePath);
+  const imageUri = panel.webview.asWebviewUri(onDiskPath).toString();
+  output += `<img src="${imageUri}" alt="${onDiskPath.path}"/>`;
+  
+  panel.webview.html = output;
+
+  thothProcess.stderr.on('exit', (data: string) => {
+    output += data;
+    panel.webview.html = output;
+  });
+
+  thothProcess.on('close', (code: number) => {
+    if (code !== 0) {
+      output += `Thoth Callgraph exited with code ${code}.`;
       panel.webview.html = output;
     }
   });
